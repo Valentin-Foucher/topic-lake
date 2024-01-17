3161cdf140f790644ae66e18608c7d7af7d59b37
Valentin <valentin.foucher@free.fr>
2024-01-17 18:48:16 +0100

    feat: automatically renew token if revoked

--- topic_recommendations/api/utils/route_utils.py
+++ topic_recommendations/api/utils/route_utils.py
@@ -8,12 +8,17 @@ from topic_recommendations.infra.repositories.users import UsersRepository
 
 
 async def ensure_authentication(request: Request, credentials: HTTPAuthorizationCredentials = Depends(HTTPBearer())):
-    user_id = AccessTokensRepository().get_user_id_for_value(credentials.credentials)
+    access_tokens_repository = AccessTokensRepository()
+    user_id, revoked = access_tokens_repository.get_user_id_for_value(credentials.credentials)
+
     if not user_id:
         raise Unauthorized('Unauthorized')
 
     user = UsersRepository().get(user_id)
     if not user:
         raise NotFound(f'User {user_id} not found')
+
+    if revoked:
+        access_tokens_repository.create(user_id)
 
     request.scope['user'] = user
